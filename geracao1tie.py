bdf5e2b8d61629fe0fad777d726d095276e022fa
patrickcferreira <patrick.c.ferreira@petrobras.com.br>
2024-05-14 09:54:45 -0300

    checkpoint: Working fine.

---
+++ geracao1tie.py
@@ -0,0 +1,46 @@
+from reduction import *
+
+G = nx.Graph()
+
+# The start node is a requirement of the script, every power source must derive from it.
+G.add_node("start", value=(0.0, 0.0))
+
+# Value is your (Lambda and MTTR).
+# 1/m = Lambda (fails per year). MTTR = mean time to repair (hours)
+G.add_node("gerador1", value=(0.1691, horas_para_anos(478)))
+G.add_node("gerador2", value=(0.1691, horas_para_anos(478)))
+G.add_node("gerador3", value=(0.1691, horas_para_anos(478)))
+G.add_node("gerador4", value=(0.1691, horas_para_anos(478)))
+G.add_node("barra11", value=(0.000125, horas_para_anos(128)))
+G.add_node("barra12", value=(0.000125, horas_para_anos(128)))
+G.add_node("barra13", value=(0.000125, horas_para_anos(128)))
+G.add_node("barra14", value=(0.000125, horas_para_anos(128)))
+G.add_node("tie12", value=(0.0096, horas_para_anos(9.6)))
+
+# alimentacao superior
+G.add_edge("start", "gerador1")
+G.add_edge("start", "gerador2")
+G.add_edge("start", "gerador3")
+G.add_edge("start", "gerador4")
+G.add_edge("gerador1", "barra11")
+G.add_edge("gerador2", "barra12")
+G.add_edge("gerador3", "barra13")
+G.add_edge("gerador4", "barra14")
+G.add_edge("barra12", "tie12")
+G.add_edge("barra13", "tie12")
+G.add_edge("barra13", "barra14")
+G.add_edge("barra11", "barra12")
+
+# Replace with <YOUR-target-node> for reliability calcs
+target_node = "barra14"
+
+# System repairable?
+repairable = True
+
+# Reduce the graph
+if repairable is True:
+    reduced_G, eq_reliability = reduce_graph(G, "start", target_node, repairable)
+    print("\n\nO valor de confiabilidade equivalente do seu sistema é: ", eq_reliability)
+else:
+    reduced_G, eq_lambda = reduce_graph(G, "start", target_node, repairable)
+    print("\n\nO valor de Lambda equivalente do seu sistema é: ", eq_lambda)
